d76ff44e07c0210c97cbc32372bb08ea494089d1
AdrianCabreraPhi <adriancabproven@gmail.com>
2026-02-27 15:43:21 +0100

    sc in header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 function Header() {
 
-
   return (
     <>
-      <h1 className="text-2xl   drop-shadow-xl  bg-gradient-to-r text-transparent bg-clip-text from-green-600 via-green-700 to-green-950 font-semibold">
-     GenoTox
-      </h1>
+      <div className="header  flex flex-row justify-center  rounded-b-xl transition-all ease-in-out duration-300 pt-2 ">
+        <h1 className="text-2xl   drop-shadow-xl  bg-gradient-to-r text-transparent bg-clip-text from-green-600 via-green-700 to-green-950 font-semibold">
+          GenoTox
+        </h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
