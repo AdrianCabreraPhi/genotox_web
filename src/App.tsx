85cb16c073430f22d79365c467d83cd416d2da3b
AdrianCabreraPhi <adriancabproven@gmail.com>
2026-02-27 17:07:36 +0100

    improved code in app

--- src/App.tsx
+++ src/App.tsx
@@ -3,119 +3,41 @@ import Header from "./components/Header";
 import FormDataQuery from "./components/FormData";
 import { ToastContainer } from "react-toastify";
 import { useState } from "react";
-import * as genotoxApi from "./service/genotoxService";
-import { toast } from "react-toastify";
+
 import VisualizeData from "./components/VisualizeData";
-import saveBlobAsExcel from "./utils/saveBlobAsExcel";
+
 import PieChart from "./components/pieChart";
 import { AnimatePresence, motion } from "framer-motion";
 import ModalInformationDatabase from "./components/ModalInformationDatabase";
 import Footer from "./components/Footer";
-import Loader from "./components/Loader";
+import useDownloadData from "./hooks/useDownloadData";
+import ButtonDownloadData from "./components/ButtonDownloadData";
+
 
 function App() {
-  const [cas_rn, setCASRN] = useState<string>("");
-  const [showDetails, setShowDetails] = useState<boolean>(false);
-  const [, setProgressQuery] = useState<number>(0);
   const [result, setResult] = useState<object | boolean>(false);
+  const [submittedCas, setSubmittedCas] = useState("");
   const [hoverEffectDatabases, setHoverEffectDatabases] = useState<object>({});
-  const [isLoading, setLoading] = useState<boolean>(false);
   const [selectedDatabase, setSelectedDatabase] = useState<string | undefined>(
-    undefined
+    undefined,
   );
+  const {downloadData} = useDownloadData();
+
+  const handleSubmittedCasNum = (casNum:string) => {
+    setSubmittedCas(casNum)
+  }
+
+  const handleDownloadData = () => {
+    downloadData(submittedCas,result)
+  }
 
   const handleChangeDatabase = (
-    event: React.ChangeEvent<HTMLSelectElement>
+    event: React.ChangeEvent<HTMLSelectElement>,
   ) => {
     setSelectedDatabase(event.target.value);
   };
 
-  const onChangeInputcasrn = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCASRN(event.target.value);
-  };
 
-  // Form
-  const submit = async () => {
-    setLoading(true);
-    setResult(false);
-    let siguePidiendoProgreso = true;
-
-    const toastId = toast.loading("Processing data 0%");
-
-    async function sondearProgreso() {
-      if (!siguePidiendoProgreso) return;
-
-      const resultado = await genotoxApi.queryProgress(cas_rn);
-      const progress = Number(resultado.data.progress);
-
-      setProgressQuery(progress);
-      toast.update(toastId, { render: `Processing data ${progress}%` });
-
-      if (progress < 100) {
-        setTimeout(sondearProgreso, 1000);
-      }
-    }
-
-    async function iniciarProceso() {
-      const formData = new FormData();
-      formData.append("cas_rn", cas_rn);
-      formData.append("details", showDetails.toString());
-
-      try {
-        setTimeout(sondearProgreso, 500);
-
-        const result = await genotoxApi.query(formData);
-
-        if (!result.data.download_ready) {
-          toast.update(toastId, {
-            render: result.data.error,
-            type: "error",
-            isLoading: false,
-            autoClose: 3000,
-          });
-          siguePidiendoProgreso = false;
-          setLoading(false);
-          return;
-        }
-
-        siguePidiendoProgreso = false;
-
-        toast.update(toastId, {
-          render: "Successfully",
-          type: "success",
-          isLoading: false,
-          autoClose: 2000,
-        });
-        setProgressQuery(100);
-        console.log("VER QUE TIPO:");
-        console.log(result.data.data);
-        setResult(result.data.data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        siguePidiendoProgreso = false;
-        setLoading(false);
-
-        toast.update(toastId, {
-          render: "Error",
-          type: "error",
-          isLoading: false,
-          autoClose: 3000,
-        });
-      }
-    }
-
-    iniciarProceso();
-  };
-
-  const downloadData = async () => {
-    const formData = { cas_rn: cas_rn, data: result };
-    const response = await genotoxApi.downloadData(formData);
-    const blob = new Blob([response.data], {
-      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    });
-    saveBlobAsExcel(blob, `gt_result_${cas_rn}.xlsx`);
-  };
 
   return (
     <>
@@ -124,18 +46,14 @@ function App() {
           <Header />
         </div>
         <div className="components  flex flex-col bg-white w-auto transition-all ease-in-out duration-300 shadow-2xl flex-grow m-8 rounded-md">
+            <div className="relative group flex flex-col md:flex-row justify-between w-full px-4 py-4 gap-4 items-start bg-neutral-50   rounded-t-md">
           <FormDataQuery
-            isLoading={isLoading}
-            downloadData={downloadData}
-            result={result}
-            submit={submit}
-            onChangeInputcasrn={onChangeInputcasrn}
-            setShowDetails={setShowDetails}
-            showDetails={showDetails}
+            setResult={setResult}
+            handleSubmittedCasNum={handleSubmittedCasNum}
           />
-
-          {isLoading && <Loader />}
-
+          <ButtonDownloadData downloadData={handleDownloadData} />
+          </div>
+          
           <div className="flex-1  overflow-hidden">
             {result && (
               <>
@@ -147,9 +65,9 @@ function App() {
                   handleChangeDatabase={handleChangeDatabase}
                 />
                 <PieChart
-                  cas_rn={cas_rn}
+                  casNum={submittedCas}
                   setHoverEffectDatabases={setHoverEffectDatabases}
-                />
+                /> 
               </>
             )}
           </div>
